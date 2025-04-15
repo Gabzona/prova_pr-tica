01ab7106d1d1385b2697afb94590dfe8eecdcbc3
GABRIELLA FRANCISCO <gabriella_francisco@estudante.sesisenai.org.br>
2025-04-15 09:57:26 -0300

    deletei o conteudo da entidade nave e vou refazer; criei as entidades de alien e a nave.

--- script.js
+++ script.js
@@ -3,12 +3,17 @@ const ctx = canvas.getContext('2d');
 
 document.addEventListener('keydown', (e) => { //função de pular ao pressionar tecla A
     if(e.code=='A'){
-        personagi.andar_esquerda() //chama a função saltar já q não da pra mudar propriedade encapsulada
+        personagi.andar_esquerda()
     }
 })
 document.addEventListener('keydown', (e) => { //função de pular ao pressionar tecla D
     if(e.code=='D'){
-        personagi.andar_direita() //chama a função saltar já q não da pra mudar propriedade encapsulada
+        personagi.andar_direita()
+    }
+})
+document.addEventListener('keydown', (e) => { //função de atirar ao pressionar tecla espaço
+    if(e.code=='Space'){
+        personagi.atirar()
     }
 })
 
@@ -29,56 +34,32 @@ class Entidade{
 }
 
 class Nave extends Entidade{
-    #velocidade_x //encapsulando(privando) a velocidade pro jogador não alterar a velocidade no console
-    constructor(x, y, largura, altura, cor){
-        super(x, y, largura, altura, cor)
-        this.#velocidade_x = 0 //pulo do personagem
-        this.andando = false //personagem está parado
-    }
-    andar_esquerda(){
-        this.x -= this.#velocidade_x
-        this.andando = true
-    }
-    atualizar(){
-        if(this.andando){
-            this.x -= this.#velocidade_x
-            this.#velocidade_x -= Jogo.gravidade
-            if(this.x >= canvas.width - 50){
-                this.#velocidade_x = 0
-                this.x = canvas.width - 50
-                this.andando = false
-            }
-        }
-    }
-    verificaColisao() {
-        if (
-            obstacolo.x < personagi.x + personagi.largura &&
-            obstacolo.largura + obstacolo.x > personagi.x &&
-            personagi.y < obstacolo.y + obstacolo.altura &&
-            personagi.y + personagi.altura > obstacolo.y
-        ) {
-            obstacolo.velocidade_x = 0;
-            personagi.velocidade_y = 0;
-            ctx.fillStyle = "black";
-            ctx.font = '50px Arial';
-            ctx.fillText('GAME OVER', 50, 100);
-            Jogo.gameOver = true;
-        }
-    }
-    desenhar(){
-        ctx.drawImage(
-            this.imagem,
-            this.x,
-            this.y,
-            this.largura,
-            this.altura,
-        )
-    }
 }
 
 class Alien extends Entidade{}
 
 class Tiro extends Entidade{}
 
-class Jogo{}
+class Jogo{
+    static gravidade = 0.5
+    static gameOver = false
+    constructor(){
+        this.loop = this.loop.bind(this)
+    }
+    loop () {
+        if(Jogo.gameOver == false){
+        ctx.clearRect(0,0, canvas.width, canvas.height)
+        alens.desenhar()
+        personagi.desenhar()
+        personagi.verificaColisao()
+        alens.atualizar()
+        personagi.atualizar()
+        requestAnimationFrame(this.loop)
+        }
+    }
+}
 
+const personagi = new Nave(375, 345, 50, 50, 'white')
+const alens = new Alien(375, 5, 50, 50, 'red')
+const jogo = new Jogo()
+jogo.loop()
