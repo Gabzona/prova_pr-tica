f3b85bb4bf1237ef5c558e65995279e6f7012fc4
GABRIELLA FRANCISCO <gabriella_francisco@estudante.sesisenai.org.br>
2025-04-15 11:20:25 -0300

    fazendo alterações na classe Nave e fazendo a classe Alien

--- script.js
+++ script.js
@@ -4,16 +4,19 @@ const ctx = canvas.getContext('2d');
 document.addEventListener('keydown', (e) => { //função de andar ao pressionar tecla A
     if(e.code=='A'){
         personagi.andar_esquerda()
+        console.log(A)
     }
 })
 document.addEventListener('keydown', (e) => { //função de andar ao pressionar tecla D
     if(e.code=='D'){
         personagi.andar_direita()
+        console.log(D)
     }
 })
 document.addEventListener('keydown', (e) => { //função de atirar ao pressionar tecla espaço
     if(e.code=='Space'){
         personagi.atirar()
+        console.log(espaço)
     }
 })
 
@@ -40,26 +43,55 @@ class Nave extends Entidade{
         this.andando = false //o personagem está parado
         this.imagem = new Image()
         this.imagem.src = '../' //adicionar imagem depois
-        this.#velocidade_x = 3
+        this.#velocidade_x = 0
     }
     andar_esquerda(){
         andando = true
-        this.x -= 3
+        this.#velocidade_x = -15
+
     }
     andar_direita(){
         andando = true
+        this.#velocidade_x = 15
+
     }
-    atirar(){}
-    atualizar(){
-        if(andando == true){}
+    atirar(){
+        
+    }
+    atualizar(){}
+    verificaColisao(){
+        if (
+            alens.y > personagi.y + personagi.altura &&
+            alens.altura + alens.y < personagi.y
+        ) {
+            alens.velocidade_y = 0;
+            personagi.velocidade_x = 0;
+            ctx.fillStyle = "black";
+            ctx.font = '50px Arial';
+            ctx.fillText('GAME OVER', 50, 100);
+            Jogo.gameOver = true;
+        }
     }
 }
 
 class Alien extends Entidade{
-    
+    #velocidade_y
+    constructor(x, y, largura, altura, cor){
+        super(x, y, largura, altura, cor)
+        this.#velocidade_y = 3 //movimento do alien descendo
+        this.vivo = true //não foi atingido
+    }
+    atualizar(){
+        this.y += this.#velocidade_y
+        if(this.y > 350){
+            let nova_posicao = Math.random() * 100
+        }
+    }
 }
 
-class Tiro extends Entidade{}
+class Tiro extends Entidade{
+
+}
 
 class Jogo{
     static gravidade = 0.5
@@ -81,6 +113,6 @@ class Jogo{
 }
 
 const personagi = new Nave(375, 345, 50, 50, 'white')
-const alens = new Alien(375, 5, 50, 50, 'red')
+const alens = [new Alien(375, 5, 50, 50, 'red')]
 const jogo = new Jogo()
 jogo.loop()
